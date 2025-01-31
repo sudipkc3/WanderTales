0a8f1d342544698f8e8d62c3556ea036597c4e3d
Sudeepkaycee289 <sudipkc289@gmail.com>
2025-01-31 23:45:58 +0545

    Updated

---
+++ Frontend/travel-story-app/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Home/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" exact element={<Home />}></Route>
+          <Route path="/login" exact element={<Login />}></Route>
+          <Route path="/signup" exact element={<SignUp />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
